5f2396efbb77d8291d5984a67c07b5d413da8e67
samuel-0214 <samuelmathe0214@gmail.com>
2025-03-14 21:11:33 +0530

    No playagain

--- app/src/app/play/route.ts
+++ app/src/app/play/route.ts
@@ -149,25 +149,25 @@ export const POST = async (req: Request) => {
                 description: `Game Over! Final Score: ${currentScore} runs 🎯`,
                 links: {
                   actions: [
-                    {
-                      type: "transaction",
-                      label: "Play Again",
-                      parameters: [
-                        {
-                          type: "radio",
-                          name: "options",
-                          options: [
-                            { label: "Play 1", value: "1", selected: false },
-                            { label: "Play 2", value: "2", selected: false },
-                            { label: "Play 3", value: "3", selected: false },
-                            { label: "Play 4", value: "4", selected: false },
-                            { label: "Play 5", value: "5", selected: false },
-                            { label: "Play 6", value: "6", selected: false },
-                          ],
-                        },
-                      ],
-                      href: `/play/`, // Restart game
-                    },
+                    // {
+                    //   type: "transaction",
+                    //   label: "Play Again",
+                    //   parameters: [
+                    //     {
+                    //       type: "radio",
+                    //       name: "options",
+                    //       options: [
+                    //         { label: "Play 1", value: "1", selected: false },
+                    //         { label: "Play 2", value: "2", selected: false },
+                    //         { label: "Play 3", value: "3", selected: false },
+                    //         { label: "Play 4", value: "4", selected: false },
+                    //         { label: "Play 5", value: "5", selected: false },
+                    //         { label: "Play 6", value: "6", selected: false },
+                    //       ],
+                    //     },
+                    //   ],
+                    //   href: `/play/`, // Restart game
+                    // },// 
                   ] 
                 },
               }
